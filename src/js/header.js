a593116e8dfb6f855e2add598bafe2fa9ae4dfdb
Sven Schoenung <sven.schoenung@gmail.com>
2017-01-13 22:48:26 +0100

    Use potraced SVG instead of JPG for header

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,7 @@
 class Header extends React.Component {
   render() {
     return (
-      <header><img alt="Karl Jaspers" src="imgs/karl-jaspers.jpg"/></header>
+      <header><img alt="Karl Jaspers" src="imgs/karl-jaspers.svg"/></header>
     );
   }
 }
