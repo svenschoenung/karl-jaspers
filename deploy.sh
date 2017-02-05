6b91d2c8f6281e6794c5619299ad7fa3fbfd7acb
Sven Schoenung <sven.schoenung@gmail.com>
2017-02-05 18:30:14 +0100

    Deploy to karl-jaspers.com host

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ set -o nounset
 set -o pipefail
 
 echo "Deleting old deployment directory"
-sshpass -e ssh $SSHUSER@schoenung.org "rm -rf /var/www/karl-jaspers.com-deploy"
+sshpass -e ssh $SSHUSER@karl-jaspers.com "rm -rf /var/www/karl-jaspers.com-deploy"
 
 echo "Copying deployment directory to server"
-sshpass -e scp -r dist $SSHUSER@schoenung.org:/var/www/karl-jaspers.com-deploy
+sshpass -e scp -r dist $SSHUSER@karl-jaspers.com:/var/www/karl-jaspers.com-deploy
 
 echo "Running deployment script on server"
-sshpass -e ssh $SSHUSER@schoenung.org "./deploy.sh karl-jaspers.com"
+sshpass -e ssh $SSHUSER@karl-jaspers.com "./deploy.sh karl-jaspers.com"
