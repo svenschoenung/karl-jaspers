cb6c28c610ee44667812754fee29529dc5837171
Sven Schoenung <sven.schoenung@gmail.com>
2017-02-11 10:21:25 +0100

    Move EditionPreview to its own file

--- src/js/client/Edition.jsx
+++ src/js/client/Edition.jsx
@@ -5,7 +5,7 @@ import SmallHeaderComponent from './SmallHeaderComponent.jsx';
 import { editionDesc } from './util.js';
 import data from '../data.json';
 
-import Lightbox from 'react-images';
+import EditionPreview from './EditionPreview.jsx';
 
 function editionPath(params) {
   var path = params.editionName
@@ -14,96 +14,6 @@ function editionPath(params) {
     path += '/' + params.editionNum;
   }
   return path;
-}
-
-class EditionPreview extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {lightboxIsOpen: false, currentImage: 0};
-  }
-
-  openLightbox(currentImage) {
-    this.setState({lightboxIsOpen: true, currentImage: currentImage });
-  }
-
-  closeLightbox() {
-    this.setState({lightboxIsOpen: false});
-  }
-
-  gotoPrevious() {
-    this.setState({ currentImage: this.state.currentImage - 1 });
-  }
-
-  gotoNext() {
-    this.setState({ currentImage: this.state.currentImage + 1 });
-  }
-
-  gotoImage(index) {
-    this.setState({
-      currentImage: index,
-    });
-  }
-
-  gotoAuto() {
-    if (this.state.currentImage == this.props.edition.images.length - 1) {
-      this.setState({currentImage: 0});
-      return;
-    }
-
-    this.gotoNext();
-  }
-
-  render() {
-    var edition = this.props.edition;
-
-    if (edition.images[0]) {
-      var imageBase = '/ausgaben/' + edition.id + '/';
-      var bigPreviewImage = imageBase + edition.images[0][200];
-      var smallPreviewImages = edition.images.slice(1).map(image => 
-        imageBase + image[100]
-      );
-      var lightboxImages = edition.images.map(image => ({
-        src: imageBase + image[0],
-        thumbnail: imageBase + image[100],
-        caption: (/umschlag/.test(image[0])) ? 'Umschlag' :
-                 (/einband/.test(image[0])) ? 'Einband' :
-                 (/cover/.test(image[0])) ? 'Cover' :
-                 (/titelseite/.test(image[0])) ? 'Titelseite' : null
-      }));
-      return (
-        <div className="edition-preview">
-          <img className="big-preview"
-               src={bigPreviewImage}
-               onClick={this.openLightbox.bind(this, 0)}/>
-
-          <div>
-          {smallPreviewImages.map((smallPreviewImage, i) =>
-            <img className="small-preview"
-                 src={smallPreviewImage}
-                 onClick={this.openLightbox.bind(this, i+1)}/>
-          )}
-          </div>
-
-          <Lightbox
-            images={lightboxImages}
-            currentImage={this.state.currentImage}
-            isOpen={this.state.lightboxIsOpen}
-            onClose={this.closeLightbox.bind(this)}
-            onClickNext={this.gotoNext.bind(this)}
-            onClickPrev={this.gotoPrevious.bind(this)}
-            onClickImage={this.gotoAuto.bind(this)}
-            onClickThumbnail={this.gotoImage.bind(this)}
-            showThumbnails={true}
-            showImageCount={false}
-            leftArrowTitle="Vorheriges Bild"
-            rightArrowTitle="Nächstes Bild"
-            closeButtonTitle="Schließen"
-          />
-        </div>
-      );
-    }
-    return <span className="edition-preview">?</span>;
-  }
 }
 
 class Edition extends SmallHeaderComponent {
