f1b56e8120ee584bfe9266e500b5bfc22041d152
Sven Schoenung <sven.schoenung@gmail.com>
2017-02-13 18:38:26 +0100

    Add external links for works

--- src/js/client/ExternalLinks.jsx
+++ src/js/client/ExternalLinks.jsx
@@ -4,24 +4,25 @@ import { Link } from 'react-router';
 import data from '../data.json';
 
 var linkDescs = {
-  dnb: 'Deutsche Nationalbibliothek',
-  google: 'Google Books',
-  scribd: 'Scribd',
-  openlib: 'Open Library',
-  jstor: 'JSTOR',
-  springer: 'Springer Link'
+  dnb: () => 'Deutsche Nationalbibliothek',
+  google: () => 'Google Books',
+  scribd: () => 'Scribd',
+  openlib: () => 'Open Library',
+  jstor: () => 'JSTOR',
+  springer: () => 'Springer Link',
+  wiki: (l) => 'Wikipedia: ' + l.url.replace(/.*\//, '').replace(/_/g, ' ')
 };
 
 export default class ExternalLinks extends React.Component {
   render() {
     var links = [];
-    Object.keys(this.props.links || {}).forEach(type => 
-      this.props.links[type].forEach(url => links.push({
-        type: type,
-        url: url,
-        desc: linkDescs[type]
-      }))
-    );
+    Object.keys(this.props.links || {}).forEach(type => {
+      this.props.links[type].forEach(url => {
+        var link = { type: type, url: url };
+        link.desc = linkDescs[type](link);
+        links.push(link)
+      });
+    });
     links = links.sort((link1, link2) => link1.desc.localeCompare(link2.desc));
 
     if (links.length == 0) {
