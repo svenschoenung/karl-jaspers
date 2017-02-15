5e0dd44bda5d8bc112e199c106c903359cf15a2c
Sven Schoenung <sven.schoenung@gmail.com>
2017-02-15 19:53:20 +0100

    Add headings to edition info

--- src/js/client/EditionInfo.jsx
+++ src/js/client/EditionInfo.jsx
@@ -7,17 +7,36 @@ export default class EditionInfo extends React.Component {
     var edition = this.props.edition;
     return (
       <div className="info">
-        <EditionDesc edition={edition} delim=", " trailingDelim={true}/>
-        {edition.year}<br/>
-        {edition.publisher}
-        {(edition.publisher_city) ? ' (' + edition.publisher_city + ')': null}
-        <br/>
-        <br/>
-        {edition.pages} Seiten
-	{(edition.series) ? <br/> : null}
-	{(edition.series) ? <br/> : null}
-	{(edition.series) ? edition.series : null}
-        <div dangerouslySetInnerHTML={{__html: this.props.edition.notes}} />
+        <div className="info-group">
+        <h4>Veröffentlichung</h4>
+        <div>
+          <EditionDesc edition={edition} delim=", " trailingDelim={true}/>
+          {edition.year}
+        </div>
+        <div>
+          {edition.publisher}
+          {(edition.publisher_city) ? ' (' + edition.publisher_city + ')': null}
+        </div>
+        </div>
+
+        <div className="info-group">
+        <h4>Umfang</h4>
+        <div>{edition.pages} Seiten</div>
+        </div>
+
+	{(!edition.series) ? null :
+          <div className="info-group">
+          <h4>Reihe</h4>
+          <div>{edition.series}</div>
+          </div>
+        }
+
+	{(!edition.notes) ? null :
+          <div className="info-group">
+          <h4>Anmerkungen</h4>
+          <div dangerouslySetInnerHTML={{__html: edition.notes}} />
+          </div>
+        }
       </div>
     );
   }
